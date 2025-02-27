5fb01acc48a717379dacbf957a961eabdfbe033b
gonzalezbri <4717bg@gmail.com>
2025-02-26 23:25:28 -0800

    3

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const { withContentlayer } = require("next-contentlayer");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
+};
+
+module.exports = withContentlayer(nextConfig);
